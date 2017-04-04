1865398b720e112361eb7d3e9a1bd63ba74270c0
orco5 <orco5@ac.sce.ac.il>
2017-04-04 20:07:12 +0300

    World Clock GUI

---
+++ World Clock/Clock/Clock/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+        Timer t = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //timer interval
+            t.Interval = 1000;   //in milliseconds
+
+            t.Tick += new EventHandler(this.t_Tick);
+
+            //start timer when form loads
+            t.Start();  //this will use t_Tick() method
+        }
+
+        //timer eventhandler
+        private void t_Tick(object sender,EventArgs e)
+        {
+            //get current time
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+
+            //time
+            string time = "";
+
+            //padding leading zero
+            if(hh<10)
+            {
+                time += "0" + hh;
+            }
+            else
+            {
+                time += hh;
+            }
+            time += ":";
+            if (mm < 10)
+            {
+                time += "0" + mm;
+            }
+            else
+            {
+                time += mm;
+            }
+            time += ":";
+            if (ss < 10)
+            {
+                time += "0" + ss;
+            }
+            else
+            {
+                time += ss;
+            }
+
+            string date = "";
+
+            int day = DateTime.Now.Day;
+            int month = DateTime.Now.Month;
+            int year = DateTime.Now.Year;
+
+            // padding leading zero
+            if (day < 10)
+            {
+                date += "0" + day;
+            }
+            else
+            {
+                date += day;
+            }
+            date += "/";
+            if (month < 10)
+            {
+                date += "0" + month;
+            }
+            else
+            {
+                date += month;
+            }
+            date += "/"+year;
+
+            //update label
+            label1.Text = time;
+            label2.Text = date;
+
+        }
+    }
+}
