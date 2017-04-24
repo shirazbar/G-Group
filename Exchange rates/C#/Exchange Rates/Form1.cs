1bc05860060d9759bd0fc6d68e2450aab16eff45
alonakorzhnev <alonakorzhnev@gmail.com>
2017-04-24 09:32:03 +0300

    GUI C# Refactoring

--- Exchange rates/C#/Exchange Rates/Form1.cs
+++ Exchange rates/C#/Exchange Rates/Form1.cs
@@ -28,277 +28,161 @@ namespace Exchange_Rates
         private void conv_btn_Click(object sender, EventArgs e)
         {
             int amount = int.Parse(amount_txt.Text);
+
             HttpRequest req = new HttpRequest();
             HttpResponse resp;
             req.Cookies = new CookieDictionary();
+
             var urlParams = new RequestParams();
             urlParams["amount"] = amount;
+
+            bool flag = false;
           
             if (comboFrom.SelectedItem == "ILS-Israeli Shekel")
             {
                 urlParams["from"] = "ILS";
 
+                if (comboTo.SelectedItem == "ILS-Israeli Shekel") 
+                {
+                    flag = true;
+                }
                 if (comboTo.SelectedItem == "EUR-Euro")
                 {
                     urlParams["to"] = "EUR";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-                    
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "USD-US Dollar")
                 {
                     urlParams["to"] = "USD";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "GBP-British Pound")
                 {
                     urlParams["to"] = "GBP";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "JPY-Japanise Yen")
                 {
                     urlParams["to"] = "JPY";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
             }
+
             if (comboFrom.SelectedItem == "EUR-Euro")
             {
                 urlParams["from"] = "EUR";
 
+                if (comboTo.SelectedItem == "EUR-Euro")
+                {
+                    flag = true;
+                }
                 if (comboTo.SelectedItem == "ILS-Israeli Shekel")
                 {
                     urlParams["to"] = "ILS";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "USD-US Dollar")
                 {
                     urlParams["to"] = "USD";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "GBP-British Pound")
                 {
                     urlParams["to"] = "GBP";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "JPY-Japanise Yen")
                 {
                     urlParams["to"] = "JPY";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
             }
+
             if (comboFrom.SelectedItem == "USD-US Dollar")
             {
                 urlParams["from"] = "USD";
 
+                if (comboTo.SelectedItem == "USD-US Dollar")
+                {
+                    flag = true;
+                }
                 if (comboTo.SelectedItem == "ILS-Israeli Shekel")
                 {
                     urlParams["to"] = "ILS";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "EUR-Euro")
                 {
                     urlParams["to"] = "EUR";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "GBP-British Pound")
                 {
                     urlParams["to"] = "GBP";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "JPY-Japanise Yen")
                 {
                     urlParams["to"] = "JPY";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
             }
+
             if (comboFrom.SelectedItem == "GBP-British Pound")
             {
                 urlParams["from"] = "GBP";
 
+                if (comboTo.SelectedItem == "GBP-British Pound")
+                {
+                    flag = true;
+                }
                 if (comboTo.SelectedItem == "ILS-Israeli Shekel")
                 {
                     urlParams["to"] = "ILS";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "EUR-Euro")
                 {
                     urlParams["to"] = "EUR";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "US-US Dollar")
                 {
                     urlParams["to"] = "USD";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "JPY-Japanise Yen")
                 {
                     urlParams["to"] = "JPY";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
             }
+
             if (comboFrom.SelectedItem == "JPY-Japanise Yen")
             {
                 urlParams["from"] = "JPY";
 
+                if (comboTo.SelectedItem == "JPY-Japanise Yen")
+                {
+                    flag = true;
+                }
                 if (comboTo.SelectedItem == "ILS-Israeli Shekel")
                 {
                     urlParams["to"] = "ILS";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "EUR-Euro")
                 {
                     urlParams["to"] = "EUR";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "US-US Dollar")
                 {
                     urlParams["to"] = "USD";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
                 if (comboTo.SelectedItem == "GBP-British Pound")
                 {
                     urlParams["to"] = "GBP";
-
-                    resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
-                    string json = resp.ToString();
-
-                    JObject data = JObject.Parse(json);
-                    double result = (double)data["result"];
-
-                    display_txt.Text = "Converted amount: " + result;
                 }
             }
+
+            if (!flag)
+            {
+                resp = req.Get("http://localhost/site.local/www/index.php", urlParams);
+                string json = resp.ToString();
+
+                JObject data = JObject.Parse(json);
+                double result = (double)data["result"];
+
+                display_txt.Text = "Converted amount: " + result;
+            }
+
+            else 
+            {
+                display_txt.Text = "Converted amount: " + amount;
+            }            
         }
     }
 }
